1f41a2fc7172c22485b995004eeed8bf68b6a18d
Arya Rahangdale <aryarahangdale25@gmail.com>
2026-09-14 09:16:58 +0530

    Add files via upload

---
+++ Java/Calculator Assignment/Calculator.java
@@ -0,0 +1,45 @@
+
+public class Calculator {
+	
+	public int add(int n1, int n2) {
+		System.out.println("Integer Function Called");
+		return n1 + n2;
+	}
+	
+	public int substract(int n1, int n2) {
+		System.out.println("Integer Function Called");
+		return n1 - n2;
+	}
+	
+	public int multiply(int n1, int n2) {
+		System.out.println("Integer Function Called");
+		return n1 * n2;
+	}
+	
+	public int divide(int n1, int n2) {
+		System.out.println("Integer Function Called");
+		return n1 / n2;
+	}
+	
+	public float add(float n1, float n2) {
+		System.out.println("Float Function Called");
+		return n1 + n2;
+	}
+	
+	public float substract(float n1, float n2) {
+		System.out.println("Float Function Called");
+		return n1 - n2;
+	}
+	
+	public float multiply(float n1, float n2) {
+		System.out.println("Float Function Called");
+		return n1 * n2;
+	}
+	
+	public float divide(float n1, float n2) {
+		System.out.println("Float Function Called");
+		return n1 / n2;
+	}
+
+	
+}
